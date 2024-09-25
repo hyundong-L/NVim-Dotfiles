7c715130f61acde0fec90e3dff2a9afdee79eef1
Leehyundong <hyundongl@icloud.com>
2024-09-26 01:45:24 +0900

    transparency setting

--- init.lua
+++ init.lua
@@ -7,3 +7,10 @@ require("config.options")
 vim.o.clipboard = "unnamedplus"
 
 vim.cmd([[highlight Search guibg=#82AAFF guifg=#000000]])
+
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]])
